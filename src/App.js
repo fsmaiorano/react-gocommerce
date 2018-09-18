00af96da5c2635e4a455afecf4a98b249b8a0179
Fábio <fsmaiorano@gmail.com>
2018-09-18 20:27:05 -0300

    add: json-server and main page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './styles/global';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>React</h1>
-      </div>
-    );
-  }
-}
+import Main from './pages/main';
+
+const App = () => <Main />;
 
 export default App;
---
+++ src/pages/main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Main = () => (
+  <Container>
+    <h1>Main Page</h1>
+  </Container>
+);
+
+export default Main;
