21d8e0ffb8e1d8c088ba44cc6bf732b95d11e89b
Fábio <fsmaiorano@gmail.com>
2018-09-29 11:38:30 -0300

    update: product details format

--- src/components/ProductDetail/index.js
+++ src/components/ProductDetail/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
+import { Container } from './styles';
+
 class ProductDetail extends Component {
   render() {
     const { product } = this.props;
-    return <h1>{product.name}</h1>;
+    return (
+      <Container>
+        <img src={product.image} alt="product" />
+        <p className="product-name">{product.name}</p>
+        <p className="product-brand">{product.brand}</p>
+        <p className="product-price">
+                    R$&nbsp;
+          {product.price}
+        </p>
+      </Container>
+    );
   }
 }
 
---
+++ src/components/ProductDetail/styles.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    .product-name {
+        font-weight: bold;
+        margin-top: 1.5rem;
+        font-size: 1.9rem;
+    }
+
+    .product-brand {
+        margin-top: 0.8rem;
+        font-size: 1.6rem;
+        color: grey;
+    }
+
+    .product-price {
+        margin-top: 1.3rem;
+        font-weight: bold;
+        font-size: 2.2rem;
+        color: #5ee0cf;
+    }
+
+    img {
+        margin-top: 4rem;
+        width: 20rem;
+    }
+`;
+
+// export const Product = styled.div`
+//     width: 30rem;
+//     height: 46.5rem;
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     background-color: #fff;
+//     margin-top: 2rem;
+
+//     .product-name {
+//         font-weight: bold;
+//         margin-top: 1.5rem;
+//         font-size: 1.9rem;
+//     }
+
+//     .product-brand {
+//         margin-top: 0.8rem;
+//         font-size: 1.6rem;
+//         color: grey;
+//     }
+
+//     .product-price {
+//         margin-top: 1.3rem;
+//         font-weight: bold;
+//         font-size: 2.2rem;
+//         color: #5ee0cf;
+//     }
+
+//     img {
+//         margin-top: 4rem;
+//         width: 20rem;
+//     }
+// `;
