1dfaef26b4a650b9ea578b61694a3c83e8f20f66
Fábio <fsmaiorano@gmail.com>
2018-09-30 22:38:54 -0300

    update: add table on cart page

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,18 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Creators as CartActions } from '../../store/ducks/cart';
 
+import {
+  Container, Table, TableHeader, TableRow, TableData,
+} from './styles';
+
 class Cart extends Component {
-  componentDidMount() {
-    debugger;
-    const { cart } = this.props;
-  }
+    state = {
+      cartItems: null,
+    };
+
+    componentDidMount() {
+      const { cart } = this.props;
+      if (cart) {
+        this.setState({ cartItems: cart.data });
+      }
+    }
 
     removeItem = (item) => {};
 
     render() {
-      return <h1>dasdas</h1>;
+      const { cartItems } = this.state;
+      return (
+        <Container>
+          <Table>
+            <TableRow>
+              <TableHeader>PRODUTO</TableHeader>
+              <TableHeader>VALOR</TableHeader>
+              <TableHeader>QTD</TableHeader>
+              <TableHeader>SUBTOTAL</TableHeader>
+            </TableRow>
+            <TableRow>
+              {!!cartItems && cartItems.length > 0
+                ? cartItems.map(item => (
+                  <Fragment>
+                    <TableData>{item.name}</TableData>
+                    <TableData>data2</TableData>
+                    <TableData>data3</TableData>
+                    <TableData>data4</TableData>
+                  </Fragment>
+                ))
+                : null}
+            </TableRow>
+          </Table>
+        </Container>
+      );
     }
 }
 
---
+++ src/pages/cart/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+    font-size: 2.1rem;
+
+    display: flex;
+    justify-content: center;
+`;
+
+export const Table = styled.table`
+    width: 100%;
+`;
+
+export const TableHeader = styled.th`
+    font-weight: bold;
+`;
+
+export const TableRow = styled.tr``;
+
+export const TableData = styled.td`
+    text-align: center;
+    font-size: 1.9rem;
+`;
