107f9995133a3efb05959bd07baf37cf69a04fd5
Fábio <fsmaiorano@gmail.com>
2018-10-02 15:18:17 -0300

    fix: simple increment

--- src/store/sagas/cart.js
+++ src/store/sagas/cart.js
@@ -5,13 +5,13 @@ export function* addCart(action) {
   try {
     const { data } = yield select(state => state.cart);
     const index = data.findIndex(p => p.id === action.payload.product.id);
-
     if (!('quantity' in action.payload.product)) {
       action.payload.product.quantity = 1;
       data.push(action.payload.product);
-    } else {
-      debugger;
+    } else if (action.payload.quantity !== 1) {
       data[index].quantity = isNaN(action.payload.quantity) === true ? 0 : action.payload.quantity;
+    } else {
+      data[index].quantity += 1;
     }
 
     const sumItems = data.reduce((prevVal, item) => prevVal + item.price * item.quantity, 0);
